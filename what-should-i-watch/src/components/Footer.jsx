46d792209a193f0155220320376930e6a3d9611e
Harper <HarperMelissaM@gmail.com>
2025-05-20 19:51:02 -0500

    change Link to useNav

--- what-should-i-watch/src/components/Footer.jsx
+++ what-should-i-watch/src/components/Footer.jsx
@@ -1,10 +1,22 @@
+import { useNavigate } from "react-router-dom";
 const Footer = () => {
+  const navigate = useNavigate();
+
+  const handleRedirect = () => {
+    navigate("/selection");
+  };
   //Button takes you to the same place, but returns a different result depending on which one is pushed. Getters and Setters?
   return (
     <footer>
-      <button className="random-movie">Get Random Movie</button>
-      <button className="random-tv-show">Get Random TV Show</button>
-      <button className="random-all">Surprise Me!</button>
+      <button className="random-movie" onClick={handleRedirect}>
+        Get Random Movie
+      </button>
+      <button className="random-tv-show" onClick={handleRedirect}>
+        Get Random TV Show
+      </button>
+      <button className="random-all" onClick={handleRedirect}>
+        Surprise Me!
+      </button>
     </footer>
   );
 };
--- what-should-i-watch/src/components/Home.jsx
+++ what-should-i-watch/src/components/Home.jsx
@@ -1,18 +1,21 @@
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
+  const navigate = useNavigate();
+
+  const handleRedirect = () => {
+    navigate("/filterContainer");
+  };
   return (
     <div className="home">
       <div className="headerContainer">
         <h1> What Should I Watch? </h1>
       </div>
       <div className="howTo">
-        <p>
-          <p> How to...</p>
-          <Link to="/filterContainer">
-            <button> Start Now </button>
-          </Link>
-        </p>
+        <p> How to...</p>
+        <div className="startNow">
+          <button onClick={handleRedirect}> Start Now </button>
+        </div>
       </div>
     </div>
   );

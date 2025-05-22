e17860deee6fce1c4c58dd251966f10c6f619e35
Harper <HarperMelissaM@gmail.com>
2025-05-22 14:55:59 -0500

    init functioning filter

--- what-should-i-watch/src/components/Filter.jsx
+++ what-should-i-watch/src/components/Filter.jsx
@@ -1,14 +1,39 @@
+// const Filter = () => {
+//   return (
+//     //<div className="filter" key={id}>
+//     <div>
+//       <p>
+//         {" "}
+//         Filter Structure Goes Here
+//         <br />
+//         Child of FilterContainer?
+//       </p>
+//     </div>
+//   );
+// };
+
+import React from "react";
+import Select from "react-select";
+import makeAnimated from "react-select/animated";
+import { genreOptions } from "../shared/utils";
+
 const Filter = () => {
+  // const genres = genreData.map((obj) => {
+  //   return (obj.value = obj.name), (obj.label = obj.name);
+  // });
+  // const { name } = genres;
+  const animatedComponents = makeAnimated();
+
   return (
-    //<div className="filter" key={id}>
-    <div>
-      <p>
-        {" "}
-        Filter Structure Goes Here
-        <br />
-        Child of FilterContainer?
-      </p>
-    </div>
+    <Select
+      closeMenuOnSelect={false}
+      components={animatedComponents}
+      isMulti
+      name="genres"
+      options={genreOptions}
+      className="basic-multi-select"
+      classNamePrefix="select"
+    />
   );
 };
 
--- what-should-i-watch/src/components/FilterContainer.jsx
+++ what-should-i-watch/src/components/FilterContainer.jsx
@@ -1,14 +1,10 @@
 import "../styles/filterContainer.css";
+import Filter from "./Filter.jsx";
 
 function FilterContainer() {
   return (
-    <div className="container-filter">
-      <p>
-        Code for rendering options container goes here. Don't forget to link to
-        the recommendation page. Getter and Setter for filtered list that should
-        then use the selectRandom function to return 5 options to the
-        recommendations page. Don't forget to specify 5 in the function call.
-      </p>
+    <div className="filter-container">
+      <Filter />
     </div>
   );
 }

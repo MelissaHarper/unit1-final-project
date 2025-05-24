7f71d420435219a13928603c46a3af2f30cc799a
Harper <HarperMelissaM@gmail.com>
2025-05-24 13:04:25 -0500

    prelim structure and style to selection

--- what-should-i-watch/src/components/Selection.jsx
+++ what-should-i-watch/src/components/Selection.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import posterFW from "../assets/images/stand-in-movie-poster.png";
+import "../styles/Selection.css";
+import dummyData from "../assets/data/dummyData.json";
 
 const Selection = () => {
-  return <div>Displays movie poster title length etc.</div>;
+  const data = dummyData.map((obj) => {
+    return { ...obj };
+  });
+
+  const { title, release_data, overview, original_title, genre_ids } = data;
+
+  return (
+    <div className="selection-wrapper">
+      <img className="poster-fw" src={posterFW} alt="Generic movie poster" />
+      <section className="selection-info">
+        <h2>{title}</h2>
+      </section>
+    </div>
+  );
 };
 
 export default Selection;

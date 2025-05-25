de41d220094f8350f2b6762b81e4fbe6490a453f
Harper <HarperMelissaM@gmail.com>
2025-05-25 18:37:17 -0500

    route paths and useState

--- what-should-i-watch/src/App.jsx
+++ what-should-i-watch/src/App.jsx
@@ -1,4 +1,4 @@
-import "./App.css";
+import { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import FilterContainer from "./components/FilterContainer";
@@ -6,8 +6,18 @@ import Home from "./components/Home";
 import About from "./components/About";
 import Footer from "./components/Footer";
 import Selection from "./components/Selection";
+import Recommendations from "./components/Recommendations";
+import data from "./assets/data/mergedDummyData.json";
+import "./App.css";
 
 function App() {
+  // State variable for selectedMovie
+  const [selectedMovie, setSelectedMovie] = useState(
+    data.map((obj) => {
+      return { ...obj };
+    })
+  );
+
   return (
     <main>
       <div className="App">
@@ -16,9 +26,26 @@ function App() {
           <Routes>
             <Route index element={<Home />} />
             <Route path="/about" element={<About />} />
+            <Route path="/recommendations" element={<Recommendations />} />
             <Route path="/filterContainer" element={<FilterContainer />} />
-            <Route path="/selection" element={<Selection />} />
-            <Route path="/footer" element={<Footer />} />
+            <Route
+              path="/selection"
+              element={
+                <Selection
+                  selectedMovie={selectedMovie}
+                  setSelectedMovie={setSelectedMovie}
+                />
+              }
+            />
+            <Route
+              path="/footer"
+              element={
+                <Footer
+                  selection={selectedMovie}
+                  setSelectedMovie={setSelectedMovie}
+                />
+              }
+            />
           </Routes>
           <Footer />
         </BrowserRouter>

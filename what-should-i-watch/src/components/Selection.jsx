07b8630f212bfa7ff090e22a661f7dc2235c9ba6
Harper <HarperMelissaM@gmail.com>
2025-05-25 17:16:44 -0500

    genral layout

--- what-should-i-watch/src/components/Selection.jsx
+++ what-should-i-watch/src/components/Selection.jsx
@@ -1,20 +1,21 @@
 import React from "react";
 import posterFW from "../assets/images/stand-in-movie-poster.png";
+import data from "../assets/data/mergedDummyData.json";
 import "../styles/Selection.css";
-import dummyData from "../assets/data/dummyData.json";
 
-const Selection = () => {
-  const data = dummyData.map((obj) => {
-    return { ...obj };
-  });
-
-  const { title, release_data, overview, original_title, genre_ids } = data;
+const Selection = ({ selection }) => {
+  let { title, release_data, overview, original_title, genres, altTitle } =
+    data[1];
 
   return (
     <div className="selection-wrapper">
       <img className="poster-fw" src={posterFW} alt="Generic movie poster" />
       <section className="selection-info">
         <h2>{title}</h2>
+        {altTitle && <h3>Original Title: {original_title}</h3>}
+        <p className="release-date">Release date: {release_data}</p>
+        <p className="overview">Overview {overview}</p>
+        <p className="genres">Genres: {genres.sort().join(", ")}</p>
       </section>
     </div>
   );

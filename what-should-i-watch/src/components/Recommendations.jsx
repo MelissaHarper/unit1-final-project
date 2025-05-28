a9843b63db9acd47a213516fb2184d11da461bb4
Harper <HarperMelissaM@gmail.com>
2025-05-27 22:47:00 -0500

    added movie card component

--- what-should-i-watch/src/components/Recommendations.jsx
+++ what-should-i-watch/src/components/Recommendations.jsx
@@ -1,10 +1,10 @@
+import MovieCard from "./MovieCard";
+
 const Recommendations = () => {
   return (
-    <p>
-      Code for listing 5 recommendations goes here. Should return 5 options.
-      maybe recommendations is the container and ViewSelection does double duty
-      as the 5 recommendation cards as well as the selection.
-    </p>
+    <div>
+      <MovieCard />
+    </div>
   );
 };
 

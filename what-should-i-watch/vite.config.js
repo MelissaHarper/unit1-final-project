51cce3ceb06122482fa48ec3bd4f772cd1abbc6d
Harper <HarperMelissaM@gmail.com>
2025-06-01 13:28:35 -0500

    resolved slow loading issues

--- what-should-i-watch/vite.config.js
+++ what-should-i-watch/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,6 +9,12 @@ export default defineConfig({
     alias: {
       // /esm/icons/index.mjs only exports the icons statically, so no separate chunks are created
       "@tabler/icons-react": "@tabler/icons-react/dist/esm/icons/index.mjs",
+      "@": path.resolve("./src/"),
+      components: `${path.resolve("./src/components/")}`,
+      interfaces: `${path.resolve("./src/interfaces/")}`,
+      services: `${path.resolve("./src/services/")}`,
+      stores: `${path.resolve("./src/stores/")}`,
+      views: `${path.resolve("./src/views/")}`,
     },
   },
 });

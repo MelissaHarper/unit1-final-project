1fa8c3cb232622dfa0065ed9b47108eeeb70fc73
Harper <HarperMelissaM@gmail.com>
2025-06-17 08:31:53 -0500

    update about page

--- what-should-i-watch/src/components/About.jsx
+++ what-should-i-watch/src/components/About.jsx
@@ -1,6 +1,8 @@
 const About = () => {
   <div className="about">
-    <p>Explanation of how to use the site</p>
+    <p>Take the scrolling out of movie selection.</p>
+    <p>Click the "Surprise Me!" button to get 5 random movie options.</p>
+    <p>Click one of the options to see cast and trailers.</p>
   </div>;
 };
 

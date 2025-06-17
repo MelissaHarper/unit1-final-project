8d7bde7fece7b7b19d078fc309a9212f0fbdf19d
Harper <HarperMelissaM@gmail.com>
2025-06-17 08:32:59 -0500

    add tailwind plugin/dependency

--- what-should-i-watch/vite.config.js
+++ what-should-i-watch/vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       // /esm/icons/index.mjs only exports the icons statically, so no separate chunks are created

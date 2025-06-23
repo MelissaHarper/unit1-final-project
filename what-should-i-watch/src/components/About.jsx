8a16a04e7583c2e791c55a2f2dd526646bed35f6
Harper <HarperMelissaM@gmail.com>
2025-06-23 01:45:09 -0500

    styling

--- what-should-i-watch/src/components/About.jsx
+++ what-should-i-watch/src/components/About.jsx
@@ -2,7 +2,9 @@ function About() {
   return (
     <div className="about">
       <p>Take the scrolling out of movie selection.</p>
-      <p>Click the "Surprise Me!" button to get 5 random movie options.</p>
+      <p>
+        Click the "Surprise Me!" button to get 5 random movie recommendations.
+      </p>
     </div>
   );
 }

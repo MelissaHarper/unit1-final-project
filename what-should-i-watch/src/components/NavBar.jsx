a74febc697f30f60922cdeced9d5777378db928b
Harper <HarperMelissaM@gmail.com>
2025-06-22 23:45:26 -0500

    added easter egg

--- what-should-i-watch/src/components/NavBar.jsx
+++ what-should-i-watch/src/components/NavBar.jsx
@@ -1,12 +1,24 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 import Logo from "../assets/images/logo.png";
+import Logo2 from "../assets/images/logo2.png";
 import "../styles/NavBar-Footer.css";
 
 function NavBar() {
+  const [image, setImage] = useState(Logo);
+
+  function handleClick() {
+    if (image === Logo) {
+      setImage(Logo2);
+    } else {
+      setImage(Logo);
+    }
+  }
+
   return (
     <div className="navbar">
       <div className="leftSide"></div>
-      <img className="nav-logo" src={Logo} />
+      <img className="nav-logo" src={image} onClick={handleClick} />
       <p className="siteName">What Should I Watch</p>
       <div className="rightSide"></div>
       <Link to="/">Home</Link>

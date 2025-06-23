61aa5868a58a98185057713d13b53cce71f30f5e
Harper <HarperMelissaM@gmail.com>
2025-06-23 01:03:16 -0500

    adjust styling

--- what-should-i-watch/src/components/About.jsx
+++ what-should-i-watch/src/components/About.jsx
@@ -1,8 +1,10 @@
-const About = () => {
-  <div className="about">
-    <p>Take the scrolling out of movie selection.</p>
-    <p>Click the "Surprise Me!" button to get 5 random movie options.</p>
-  </div>;
-};
+function About() {
+  return (
+    <div className="about">
+      <p>Take the scrolling out of movie selection.</p>
+      <p>Click the "Surprise Me!" button to get 5 random movie options.</p>
+    </div>
+  );
+}
 
 export default About;

a7490353607d0059be011e4aabac402bae553f30
Harper <HarperMelissaM@gmail.com>
2025-07-16 10:22:44 -0500

    add clerk dependency remove tmdb wrapper dependency

--- what-should-i-watch/src/main.jsx
+++ what-should-i-watch/src/main.jsx
@@ -3,9 +3,20 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { ClerkProvider } from "@clerk/clerk-react";
+
+// Import clerk Publishable Key
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+// Throw error if key not imported
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Add your Clerk Publishable Key to the .env file");
+}
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <App />
+    </ClerkProvider>
   </StrictMode>
 );

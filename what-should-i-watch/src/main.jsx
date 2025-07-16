efb2e5268f4df9fa0a75df3545ac764b9dd4fc88
Harper <HarperMelissaM@gmail.com>
2025-07-16 14:16:40 -0500

    add ClerkProvider for user authentication.

--- what-should-i-watch/src/main.jsx
+++ what-should-i-watch/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { ClerkProvider } from "@clerk/clerk-react";
+import { ClerkProvider } from "@clerk/react-router";
 
 // Import clerk Publishable Key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;

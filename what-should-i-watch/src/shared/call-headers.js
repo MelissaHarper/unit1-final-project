838f04da4017ed303373225ca2c915c3bf244507
Harper <HarperMelissaM@gmail.com>
2025-07-17 17:37:13 -0500

    update name

--- what-should-i-watch/src/shared/call-headers.js
+++ what-should-i-watch/src/shared/call-headers.js
@@ -1,14 +1,13 @@
 import axios from "axios";
 
-const api_key = import.meta.env.VITE_API_KEY;
 const authorization = import.meta.env.VITE_AUTHORIZATION;
-const baseUrl = import.meta.env.VITE_BASE_API_URL;
+const baseUrl = import.meta.env.VITE_BASE_TMDB_API_URL;
 
 export const options = {
   method: "GET",
   headers: {
     accept: "application/json",
-    Authorization: authorization,
+    authorization: authorization,
   },
 };
 
